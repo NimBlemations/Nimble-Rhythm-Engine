cb2ec4f22ca5606809733850042bb4cb06a646e0
NimBlemations <78566097+NimBlemations@users.noreply.github.com>
2021-11-01 17:35:53 -0600

    Did stuff
    Don't remember what I did, but I did stuff.

--- FridayNightFunkinGodot/Assets/Images/Arrows/FunkinArrows.gd
+++ FridayNightFunkinGodot/Assets/Images/Arrows/FunkinArrows.gd
@@ -8,10 +8,34 @@ onready var arrowDown : Sprite = get_node("ArrowDown")
 onready var arrowUp : Sprite = get_node("ArrowUp")
 onready var arrowRight : Sprite = get_node("ArrowRight")
 onready var jsonFile = File.new()
+onready var configControlsPath = String("user://NimbleRhythmEngine/Config/controls.cfg")
+onready var config = ConfigFile.new()
 
 func _ready():
+	var controlLeft : int
+	var controlDown : int
+	var controlUp : int
+	var controlRight : int
+	var controls : int
+	#To use configged controls
+	if whichSide == "Right":
+		controls = config.load(configControlsPath)
+		if controls == OK:
+			controlLeft = config.get_value("otherKeys","controlLeft",65)
+			controlDown = config.get_value("otherKeys","controlDown",83)
+			controlUp = config.get_value("otherKeys","controlUp",87)
+			controlRight = config.get_value("otherKeys","controlRight",68)
+		else:
+			var file = File.new()
+			if file.file_exists(configControlsPath) == false:
+				config.set_value("otherKeys","controlLeft",65)
+				config.set_value("otherKeys","controlDown",83)
+				config.set_value("otherKeys","controlUp",87)
+				config.set_value("otherKeys","controlRight",68)
+				config.save(configControlsPath)
+	
 	var jsonDictionary : Dictionary
 	jsonFile.open(songJsonPath, File.READ)
-	print(JSON.parse(jsonFile.get_as_text()).result)
 	
 	jsonDictionary = JSON.parse(jsonFile.get_as_text()).result
+	var notes : Array = jsonDictionary.get("song").get("notes")
