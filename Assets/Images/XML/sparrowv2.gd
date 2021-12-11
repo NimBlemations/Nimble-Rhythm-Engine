8ecd7bed0e6751e2dcd91fcae15f9fd8e1a8fd7b
axel <axel@nimblebrain.net>
2021-12-10 23:14:32 -0700

    XML failsafe
    Up arrow good

--- Assets/Images/XML/sparrowv2.gd
+++ Assets/Images/XML/sparrowv2.gd
@@ -27,7 +27,7 @@ func xmlScan():
 					xmlRegions.insert(xmlRegions.size(), [xmlScanner.get_attribute_value(0), xmlScanner.get_attribute_value(1), xmlScanner.get_attribute_value(2), xmlScanner.get_attribute_value(3), xmlScanner.get_attribute_value(4), xmlScanner.get_attribute_value(5), xmlScanner.get_attribute_value(6)])
 			xmlScanner.read()
 
-func xmlAnim(node : String, time : float):
+func xmlAnim(node : String, time : float = 30):
 	xmlShiftFrame = Engine.get_frames_per_second() / (time * Engine.get_frames_per_second())
 	if not ".xml" in xmlSprite:
 		print(xmlSprite)
@@ -38,7 +38,7 @@ func xmlAnim(node : String, time : float):
 		xmlSprite = textureResourcePath.replace(".png", ".xml")
 		xmlScan()
 		print("Scanny")
-	print(xmlRegions.size(), ", hopefully not 0.")
+	print(xmlRegions.size(), ", hopefully not 0.", (str("\n", xmlRegions.size() - 1, " is the known maximum index.") if xmlRegions.size() != 0 else "\nYou're fucked."))
 	var ready = false
 	var frameint = null
 	for i in xmlRegions.size() - 1:
@@ -50,7 +50,7 @@ func xmlAnim(node : String, time : float):
 	if ready == true:
 		xmlShift = true
 		while node in xmlRegions[frameint][0] and xmlShift == true:
-			if xmlRegions[frameint] != null:
+			if xmlRegions[frameint]:
 				if xmlShift == false:
 					return
 				self.region_rect = Rect2(Vector2(xmlRegions[frameint][1], xmlRegions[frameint][2]), Vector2(xmlRegions[frameint][3], xmlRegions[frameint][4]))
@@ -58,7 +58,9 @@ func xmlAnim(node : String, time : float):
 					self.offset.x = -(int(xmlRegions[frameint][5]) if flip_h != true else int(xmlRegions[frameint][5]) * -1)
 					self.offset.y = -(int(xmlRegions[frameint][6]) if flip_v != true else int(xmlRegions[frameint][6]) * -1)
 				xmlShiftAnim = xmlRegions[frameint][0]
-			frameint += 1
+			# Successful failsafe!
+			if not frameint + 1 > xmlRegions.size() - 1:
+				frameint += 1
 			yield(get_tree().create_timer(Engine.get_frames_per_second() / (time * Engine.get_frames_per_second())), "timeout")
 		print("Finished .xml anim")
 	else:
